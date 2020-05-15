f214e8b196e11d704fd38516467579664f12f68e
Abraham-Vensaslas <61682246+Abraham-Vensaslas@users.noreply.github.com>
2020-05-15 17:54:52 +0530

    Update Lamb_da.py

--- Lamb_da.py
+++ Lamb_da.py
@@ -11,11 +11,9 @@ print(t(4))
 
 #filter/map/reduce
 r=[25,23,6,49,78,96,20]
-
 new_list=tuple(map(lambda a:(a/7==7),r ))
 print(new_list)
 
 r=[25,23,6,49,78,96,20]
-
 new_list=tuple(filter(lambda a:(a/7==7),r ))
 print(new_list)
